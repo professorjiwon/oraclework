a6e6b77797363498cbb5f128bd4b7f7fb3d9cd40
professorjiwon <professorjiwon@gmail.com>
2024-06-14 17:42:07 +0900

    06.14_7교시

--- 06.subQuery_tjoeun계정.sql
+++ 06.subQuery_tjoeun계정.sql
@@ -46,6 +46,7 @@ WHERE SALARY > (SELECT CEIL(AVG(SALARY))
        >> 서브쿼리의 종류가 뭐냐에 따라 서브쿼리 앞에 붙는 연산자가 달라짐
  */
 
+ -------------------------------------------------------------------------------------------------------------------
 /*
     1. 단일행 서브쿼리 : 서브쿼리의 조회 결과값이 오로지 1개 일때(1행1열)
         일반 비교연산자 사용 가능
@@ -79,6 +80,113 @@ WHERE DEPT_CODE = DEPT_ID
     AND SALARY > (SELECT SALARY
                              FROM EMPLOYEE
                              WHERE EMP_NAME = '박정보');  -- 순서 상관 없음
+
+--    >> ANSI 구문
+SELECT EMP_ID, EMP_NAME, DEPT_TITLE, SALARY
+ FROM EMPLOYEE
+   JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+WHERE SALARY > (SELECT SALARY
+                             FROM EMPLOYEE
+                             WHERE EMP_NAME = '박정보');
+                             
+-- 5) 왕정보사원과 같은 부서코드가 같은 사원들의 사번, 사원명, 전화번호, 입사일, 부서명 조회(단, 왕정보는 제외)
+--    >> 오라클전용 구문
+SELECT EMP_ID, EMP_NAME, PHONE, HIRE_DATE, DEPT_TITLE
+ FROM EMPLOYEE, DEPARTMENT
+WHERE DEPT_CODE = DEPT_ID
+   AND DEPT_CODE = (SELECT DEPT_CODE
+                                  FROM EMPLOYEE
+                                 WHERE EMP_NAME = '왕정보')
+   AND EMP_NAME != '왕정보';
+
+--    >> ANSI 구문
+SELECT EMP_ID, EMP_NAME, PHONE, HIRE_DATE, DEPT_TITLE
+ FROM EMPLOYEE
+  JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+WHERE DEPT_CODE = (SELECT DEPT_CODE
+                                  FROM EMPLOYEE
+                                 WHERE EMP_NAME = '왕정보')
+   AND EMP_NAME != '왕정보';
+   
+   
+-- GROUP BY
+-- 6) 부서별 급여합이 가장 큰 부서의 부서코드, 급여합 조회
+--      6.1 부서별 급여합 중 가장 큰 값 하나만 조회
+SELECT MAX(SUM(SALARY))
+ FROM EMPLOYEE
+GROUP BY DEPT_CODE;
+
+--     6.2 부서별 급여합이 17700000인 부서를 조회
+SELECT DEPT_CODE, SUM(SALARY)
+ FROM EMPLOYEE
+GROUP BY DEPT_CODE
+HAVING SUM(SALARY) = 17700000;
+
+-- 위의 쿼리문을 하나로
+SELECT DEPT_CODE, SUM(SALARY)
+ FROM EMPLOYEE
+GROUP BY DEPT_CODE
+HAVING SUM(SALARY) = (SELECT MAX(SUM(SALARY))
+                                       FROM EMPLOYEE
+                                      GROUP BY DEPT_CODE);
+
+ -------------------------------------------------------------------------------------------------------------------
+/*
+    2. 다중행 서브쿼리
+       : 서브쿼리의 조회 결과값이 여러행 일 때(여러행 1열)
+       - IN 서브쿼리 : 여러개의 결과값 중에서 한개라도 일치하는 값이 있다면
+          > ANY 서브쿼리 : 여러개의 결과값 중에서 "한개라도" 클 경우
+                                    (여러개의 결과값 중에서 가장 작은값 보다 클 경우)
+          < ANY 서브쿼리 : 여러개의 결과값 중에서 "한개라도" 작은 경우
+                                     (여러개의 결과값 중에서 가장 큰값 보다 작을 경우)  
+                                     
+        비교대상 > ANY (값1, 값2, 값3)
+        비교대상 > 값1 OR 비교대상 > 값2 OR 비교대상 > 값3
+*/
+-- 1) 조정연 또는 전지연 사원과 같은 직급인 사원들의 사번, 사원명, 직급코드, 급여 조회
+--    1.1 조정연 또는 전지연의 직급
+SELECT JOB_CODE
+ FROM EMPLOYEE
+WHERE EMP_NAME IN ('조정연', '전지연');
+
+--    1.2 J3, J7 직급인 사원들 조회
+SELECT EMP_ID, EMP_NAME, JOB_CODE, SALARY
+ FROM EMPLOYEE
+WHERE JOB_CODE IN ('J3', 'J7'); 
+
+-- 위의 쿼리문을 한줄로
+SELECT EMP_ID, EMP_NAME, JOB_CODE, SALARY
+ FROM EMPLOYEE
+WHERE JOB_CODE IN  (SELECT JOB_CODE
+                                   FROM EMPLOYEE
+                                 WHERE EMP_NAME IN ('조정연', '전지연'));
+
+--  사원 -> 대리 -> 과장
+-- 2) 대리 직급임에도 불구하고 과장직급 급여들 중 최소 급여보다 많이 받는 직원의 사번, 사원명, 직급, 급여 조회
+--     2.1. 과장들의 급여
+SELECT SALARY
+ FROM EMPLOYEE
+  JOIN JOB USING (JOB_CODE)
+WHERE JOB_NAME = '과장';     -- 2200, 2500, 3760
+
+--     2.2. 직급이 대리이면서 급여가 위의 목록의 값 보다 하나라도 큰 사원
+SELECT EMP_ID, EMP_NAME, JOB_NAME, SALARY
+ FROM EMPLOYEE
+ JOIN JOB USING (JOB_CODE)
+WHERE JOB_NAME = '대리' 
+    AND SALARY > ANY(2200000, 2500000, 3760000);
+
+-- 위의 쿼리문을 하나로
+SELECT EMP_ID, EMP_NAME, JOB_NAME, SALARY
+ FROM EMPLOYEE
+ JOIN JOB USING (JOB_CODE)
+WHERE JOB_NAME = '대리' 
+    AND SALARY > ANY (SELECT SALARY
+                                    FROM EMPLOYEE
+                                      JOIN JOB USING (JOB_CODE)
+                                    WHERE JOB_NAME = '과장');
+
+
 
 
 
