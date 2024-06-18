b26af5647037669d7599eb4ee4c3bcee77fd89ff
professorjiwon <professorjiwon@gmail.com>
2024-06-18 16:29:22 +0900

    06.18_6교시

--- 07.DDL(CREATE)_ddl계정.sql
+++ 07.DDL(CREATE)_ddl계정.sql
@@ -471,10 +471,98 @@ INSERT INTO MEMBER2 (MEM_NO, MEM_ID, MEM_AGE) VALUES(4, 'user04', 25);
        [표현식]
        CREATE TABLE 테이블명 AS 서브쿼리;     
 */
+-- EMPLOYEE테이블을 복제(컬럼값까지)한 새로운 테이블 생성
+CREATE TABLE EMPLOYEE_COPY
+AS SELECT *
+      FROM EMPLOYEE;
+-- 컬럼, 데이터값 복사 됨
+-- 제약조건은 NOT NULL만 복사됨. 나머지는 안됨
+--     FRIMARY KEY, DEFAULT, COMMENT...는 복사안됨
+
+-- EMPLOYEE테이블을 복제(컬럼값제외 구조만)한 새로운 테이블 생성
+CREATE TABLE EMPLOYEE_COPY2
+AS SELECT *
+      FROM EMPLOYEE
+      WHERE 1=0;
+      
+-- EMPLOYEE테이블을 복제(컬럼값제외 구조만)한 컬럼을 내가 원하는 컬럼만으로 새로운 테이블 생성
+CREATE TABLE EMPLOYEE_COPY3
+AS SELECT EMP_ID, EMP_NAME, SALARY
+      FROM EMPLOYEE
+      WHERE 1=0;
+      
+-- EMPLOYEE테이블을 복제(컬럼값제외 구조만)한 컬럼을 기준테이블에 없는 컬럼도 생성
+CREATE TABLE EMPLOYEE_COPY4
+AS SELECT EMP_ID, EMP_NAME, SALARY, SALARY*12 연봉
+      FROM EMPLOYEE;
+      -- 서브쿼리 SELECT절에 산술식 또는 함수식이 기술된 경우 반드시 별칭을 지정해야 됨
+      
+--------------------------------------------------------------------------------------------------------------
+/*
+    * 테이블 생성 후 제약조건 추가
+      ALTER TABLE 테이블명 변경할 내용
+       - PRIMARY KEY : ALTER TABLE 테이블명 ADD PRIMARY KEY(컬럼명)
+       - FOREIGN KEY : ALTER TABLE 테이블명 ADD FOREIGN KEY(컬럼명) REFERENCES 참조할테이블명 [(참조할컬럼명)]
+       - UNIQUE : ALTER TABLE 테이블명 ADD UNIQUE(컬럼명)
+       - CHECK : ALTER TABLE 테이블명 ADD CHECK(컬럼에 대한 조건식)
+       - NOT NULL : ALTER TABLE 테이블명 MODIFY 컬럼명 NOT NULL
+*/
+-- EMPLOYEE_COPY 테이블에 PRIMARY KEY 제약 조건 추가
+ALTER TABLE EMPLOYEE_COPY ADD PRIMARY KEY(EMP_ID);
+
+-- EMPLOYEE_COPY 테이블에 외래키 제약 조건 추가
+ALTER TABLE EMPLOYEE_COPY ADD FOREIGN KEY(DEPT_CODE) REFERENCES DEPARTMENT;
+
+-- COMMENT
+COMMENT ON COLUMN EMPLOYEE_COPY.EMP_ID IS '사원아이디';
 
 
+----------------------------------------------------- 연습문제 ---------------------------------------------------
+--  DDL 계정에서
+/*
+도서관리 프로그램을 만들기 위한 테이블들 만들기
+이때, 제약조건에 이름을 부여할 것.
+       각 컬럼에 주석달기
+
+1. 출판사들에 대한 데이터를 담기위한 출판사 테이블(TB_PUBLISHER)
+   컬럼  :  PUB_NO(출판사번호) NUMBER -- 기본키(PUBLISHER_PK) 
+	PUB_NAME(출판사명) VARCHAR2(50) -- NOT NULL(PUBLISHER_NN)
+	PHONE(출판사전화번호) VARCHAR2(13) - 제약조건 없음
+
+   - 3개 정도의 샘플 데이터 추가하기
 
 
+2. 도서들에 대한 데이터를 담기위한 도서 테이블(TB_BOOK)
+   컬럼  :  BK_NO (도서번호) NUMBER -- 기본키(BOOK_PK)
+	BK_TITLE (도서명) VARCHAR2(50) -- NOT NULL(BOOK_NN_TITLE)
+	BK_AUTHOR(저자명) VARCHAR2(20) -- NOT NULL(BOOK_NN_AUTHOR)
+	BK_PRICE(가격) NUMBER
+	BK_PUB_NO(출판사번호) NUMBER -- 외래키(BOOK_FK) (TB_PUBLISHER 테이블을 참조하도록)
+			         이때 참조하고 있는 부모데이터 삭제 시 자식 데이터도 삭제 되도록 옵션 지정
+   - 5개 정도의 샘플 데이터 추가하기
 
 
+3. 회원에 대한 데이터를 담기위한 회원 테이블 (TB_MEMBER)
+   컬럼명 : MEMBER_NO(회원번호) NUMBER -- 기본키(MEMBER_PK)
+   MEMBER_ID(아이디) VARCHAR2(30) -- 중복금지(MEMBER_UQ)
+   MEMBER_PWD(비밀번호)  VARCHAR2(30) -- NOT NULL(MEMBER_NN_PWD)
+   MEMBER_NAME(회원명) VARCHAR2(20) -- NOT NULL(MEMBER_NN_NAME)
+   GENDER(성별)  CHAR(1)-- 'M' 또는 'F'로 입력되도록 제한(MEMBER_CK_GEN)
+   ADDRESS(주소) VARCHAR2(70)
+   PHONE(연락처) VARCHAR2(13)
+   STATUS(탈퇴여부) CHAR(1) - 기본값으로 'N' 으로 지정, 그리고 'Y' 혹은 'N'으로만 입력되도록 제약조건(MEMBER_CK_STA)
+   ENROLL_DATE(가입일) DATE -- 기본값으로 SYSDATE, NOT NULL 제약조건(MEMBER_NN_EN)
 
+   - 5개 정도의 샘플 데이터 추가하기
+
+
+4. 어떤 회원이 어떤 도서를 대여했는지에 대한 대여목록 테이블(TB_RENT)
+   컬럼  :  RENT_NO(대여번호) NUMBER -- 기본키(RENT_PK)
+	RENT_MEM_NO(대여회원번호) NUMBER -- 외래키(RENT_FK_MEM) TB_MEMBER와 참조하도록
+			이때 부모 데이터 삭제시 자식 데이터 값이 NULL이 되도록 옵션 설정
+	RENT_BOOK_NO(대여도서번호) NUMBER -- 외래키(RENT_FK_BOOK) TB_BOOK와 참조하도록
+			이때 부모 데이터 삭제시 자식 데이터 값이 NULL값이 되도록 옵션 설정
+	RENT_DATE(대여일) DATE -- 기본값 SYSDATE
+
+   - 3개 정도 샘플데이터 추가하기
+*/
