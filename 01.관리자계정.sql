cefeb80f38f56b9b40bd99653b6a7769ddfb32ba
professorjiwon <professorjiwon@gmail.com>
2025-02-03 18:10:04 +0900

    02.03_8교시_관리자 계정

--- 01.관리자계정.sql
+++ 01.관리자계정.sql
@@ -36,4 +36,10 @@ GRANT  RESOURCE TO tjoeun;
 ALTER USER tjoeun default tablespace users quota unlimited on users;
 -- ALTER USER tjoeun quota 50M on users;
 
+-- workbook사용자 만들기
+ALTER SESSION SET "_oracle_script" = true;
+CREATE USER workbook IDENTIFIED BY 1234;
+GRANT CONNECT, RESOURCE TO workbook;
+ALTER USER workbook default tablespace users quota unlimited on users;
+
 
