beb09b3d0684d3a332e1ba9e885a9eef39c9cbcf
professorjiwon <professorjiwon@gmail.com>
2025-02-05 16:56:30 +0900

    02.05_GROUP

---
+++ 04.1.GROUP_HAVING_tjoeun계정.sql
@@ -0,0 +1,75 @@
+/*
+    <GROUP BY절>
+    여러개의 값들을 하나의 그룹으로 묶어서 처리할 목적으로 사용
+*/
+
+-- 각 부서별 총 급여액
+SELECT DEPT_CODE, SUM(SALARY)
+FROM EMPLOYEE
+GROUP BY DEPT_CODE;
+
+-- 각 부서별 사원 수
+SELECT DEPT_CODE, COUNT(*)
+FROM EMPLOYEE
+GROUP BY DEPT_CODE;
+
+SELECT DEPT_CODE, SUM(SALARY), COUNT(*)
+FROM EMPLOYEE
+GROUP BY DEPT_CODE;
+
+-- 직급별 사원수와 급여의 총합
+SELECT JOB_CODE, COUNT(*), SUM(SALARY)
+FROM EMPLOYEE
+GROUP BY JOB_CODE
+ORDER BY JOB_CODE;
+
+-- 직급별 사원수, 보너스를 받는 사원수, 급여합, 평균급여, 최저급여, 최고급여를 직급별 오름차순으로 정렬
+SELECT JOB_CODE, COUNT(*), COUNT(BONUS), SUM(SALARY), ROUND(AVG(SALARY),-1), MIN(SALARY), MAX(SALARY)
+FROM EMPLOYEE
+GROUP BY JOB_CODE
+ORDER BY JOB_CODE;
+
+-- 남,여별 사원수
+-- DECODE는 오라클에서만 사용하는 함수
+SELECT DECODE(SUBSTR(EMP_NO,8,1), '1', '남', '2', '여', '3', '남', '4', '여') "성별 사원수", COUNT(*)
+FROM EMPLOYEE
+GROUP BY DECODE(SUBSTR(EMP_NO,8,1), '1', '남', '2', '여', '3', '남', '4', '여');
+
+-- 모든 DB 다 사용
+SELECT CASE WHEN SUBSTR(EMP_NO,8,1) = '1' THEN '남'
+                    WHEN SUBSTR(EMP_NO,8,1) = '2' THEN '여'
+                    WHEN SUBSTR(EMP_NO,8,1) = '3' THEN '남'
+                    WHEN SUBSTR(EMP_NO,8,1) = '4' THEN '여'
+            END "성별 사원수"
+            , COUNT(*)
+FROM EMPLOYEE
+GROUP BY CASE WHEN SUBSTR(EMP_NO,8,1) = '1' THEN '남'
+                         WHEN SUBSTR(EMP_NO,8,1) = '2' THEN '여'
+                         WHEN SUBSTR(EMP_NO,8,1) = '3' THEN '남'
+                         WHEN SUBSTR(EMP_NO,8,1) = '4' THEN '여'
+                 END;
+
+-- GROUP BY절에 여러 컬럼 기술 가능
+SELECT DEPT_CODE, JOB_CODE, COUNT(*), SUM(SALARY)
+FROM EMPLOYEE
+GROUP BY DEPT_CODE, JOB_CODE
+ORDER BY DEPT_CODE, JOB_CODE;
+
+---------------------------------------------------------------------------------------------------------
+/*
+    <HAVING 절>
+    그룹에 대한 조건을 제시할 때 사용되는 구문
+*/
+-- 각 부서별 평균 급여가 300만원 이상인 부서들만 조회
+SELECT DEPT_CODE, CEIL(AVG(SALARY))
+FROM EMPLOYEE
+GROUP BY DEPT_CODE
+HAVING AVG(SALARY) >= 3000000;
+
+---------------------------- 실습문제------------------------------
+--1. 직급별 총 급여합(단, 직급별 급여합이 1000만원 이상인 직급만 조회) 직급코드, 급여합 조회
+
+--2. 부서별 보너스를 받는 사원이 없는 부서만 부서코드를 조회
+
+
+
