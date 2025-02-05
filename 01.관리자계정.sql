5d79a1f922e86ec9723a038f2a056e4ea5f3be7a
professorjiwon <professorjiwon@gmail.com>
2025-02-05 15:53:04 +0900

    02.06_6교시_워크북 답

--- 01.관리자계정.sql
+++ 01.관리자계정.sql
@@ -42,4 +42,10 @@ CREATE USER workbook IDENTIFIED BY 1234;
 GRANT CONNECT, RESOURCE TO workbook;
 ALTER USER workbook default tablespace users quota unlimited on users;
 
+-- scott사용자 만들기
+ALTER SESSION SET "_oracle_script" = true;
+CREATE USER scott IDENTIFIED BY 1234;
+GRANT CONNECT, RESOURCE TO scott;
+ALTER USER scott default tablespace users quota unlimited on users;
+
 
