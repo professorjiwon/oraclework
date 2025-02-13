85f7ee255601d5caa4893c36793729c1cd8f86a4
professorjiwon <professorjiwon@gmail.com>
2025-02-13 12:37:48 +0900

    02.13_3교시_PL/SQL

--- 14.1.PL_SQL_tjoeun계정.sql
+++ 14.1.PL_SQL_tjoeun계정.sql
@@ -21,7 +21,7 @@ END;
 /*
     1. DECLARE 선언부
     변수 및 상수 선언하는 공간(선언과 동시에 초기화도 가능)
-    일반타입변수, 레퍼런스 스타일 변수, ROW타입 변수
+    일반타입 변수, 레퍼런스타입 변수, ROW타입 변수
     
     1.1)  일반타입변수 선언 및 초기화
             변수명 [CONSTRANT] 자료형 [:= 값]
@@ -31,7 +31,6 @@ DECLARE
     EID NUMBER;
     ENAME VARCHAR2(20);
     PI CONSTANT NUMBER := 3.14;
-    
 BEGIN
     EID := 800;
     ENAME := '배정남';
@@ -42,7 +41,7 @@ BEGIN
 END;
 /
 
---  1.2) 레퍼런스 스타일 변수 선언 및 초기화 (테이블의 컬럼의 데이터타입을 참조하여 그타입으로 지정)
+--  1.2) 레퍼런스 타입 변수 선언 및 초기화 (테이블의 컬럼의 데이터타입을 참조하여 그타입으로 지정)
 --        변수명 테이블명.컬럼명%TYPE;
 
 DECLARE
@@ -73,7 +72,7 @@ BEGIN
     WHERE EMP_ID=201;
 */    
 
--- 사용자로부터 EMP_ID 받기
+-- 사용자로부터 EMP_ID 받기(& 키워드 사용)
    SELECT EMP_ID, EMP_NAME, SALARY
        INTO EID, ENAME, SAL
       FROM EMPLOYEE
@@ -244,3 +243,292 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('소속 : ' || TEAM);
 END;
 /
+
+/*
+-- 3) IF-ELSE IF.. 조건문
+        IF 조건식1
+            THEN 실행내용1
+        ELSIF 조건식2
+            THEN 실행내용2
+        ELSIF 조건식3
+            THEN 실행내용3
+        ELSE
+            실행내용4
+        END IF;     
+*/
+
+-- 사용자로부터 점수를 입력받아 학점출력
+-- 변수1=점수, 변수2=학점
+DECLARE
+    SCORE NUMBER;
+    GRADE VARCHAR2(1);
+BEGIN
+    SCORE := &점수;
+    
+    IF SCORE >= 90 THEN GRADE := 'A';
+    ELSIF SCORE >= 80 THEN GRADE := 'B';
+    ELSIF SCORE >= 70 THEN GRADE := 'C';
+    ELSIF SCORE >= 60 THEN GRADE := 'D';
+    ELSE GRADE := 'F';
+    END IF;
+    
+    DBMS_OUTPUT.PUT_LINE('당신의 점수는 ' || SCORE || '점 이고, 학점은 ' || GRADE || '학점입니다');
+END;
+/
+
+------------------------------------------------------- 실습 문제 ------------------------------------------------------
+-- 사용자에게 입력받은 사번인 사원의 급여를 조회하여 SAL 변수에 대입하고
+-- 5백만원 이상이면 '고급'
+-- 3백만원 이상이면 '중급'
+-- 3백만원 미만이면 '초급'
+-- 출력문 : 해당 사원의 급여 등급은 ??입니다.
+
+DECLARE
+    SAL EMPLOYEE.SALARY%TYPE;
+    GRADE VARCHAR2(10);
+BEGIN
+    SELECT SALARY
+    INTO SAL
+    FROM EMPLOYEE
+    WHERE EMP_ID = &사번;
+    
+    IF SAL >= 5000000 THEN GRADE := '고급';
+    ELSIF SAL >= 3000000 THEN GRADE := '중급';
+    ELSE GRADE := '초급';
+    END IF;
+    
+    DBMS_OUTPUT.PUT_LINE('해당 사원의 급여 등급은 ' || GRADE || '입니다');
+END;
+/
+
+/*  CASE 문
+
+-- 4) 
+        CASE 비교대상자
+            WHEN 비교할값1 THEN 실행내용1
+            WHEN 비교할값2 THEN 실행내용2
+            WHEN 비교할값3 THEN 실행내용3
+            ELSE 실행내용4
+        END;
+        
+        - 자바에서
+        SWITCH(변수) {            --> CASE
+            CASE 비교할값1 :     --> WHEN
+                실행내용1;          --> THEN
+            DEFAULT :               --> ELSE
+                실행내용2
+*/
+
+-- 사용자로부터 입력받은 사번의 사원을 부서코드를 이용하여 부서이름으로 출력하기
+DECLARE
+    EMP EMPLOYEE%ROWTYPE;
+    DNAME VARCHAR2(20);
+BEGIN
+    SELECT *
+    INTO EMP
+    FROM EMPLOYEE
+    WHERE EMP_ID = &사번;
+    
+    DNAME := CASE EMP.DEPT_CODE
+                        WHEN 'D1' THEN '인사팀'
+                        WHEN 'D2' THEN '회계관리팀'
+                        WHEN 'D3' THEN '마케팅팀'
+                        WHEN 'D4' THEN '국내영업팀'
+                        WHEN 'D8' THEN '기술지원팀'
+                        WHEN 'D9' THEN '총무팀'
+                        ELSE '해외영업팀'
+                    END;
+    DBMS_OUTPUT.PUT_LINE(EMP.EMP_NAME || '는 ' || DNAME || '입니다');
+END;
+/
+
+-- 사용자로부터 입력받은 사번의 사원을 부서코드를 이용하여 근무지역 출력하기
+-- D5 : 일본
+-- D6 : 중국
+-- D7 : 미국
+-- D8 : 러시아
+-- 나머지 : 한국
+
+--------------------------------------------------------------------------------------------------------------------
+/*
+    <LOOP>
+    1. BASIC LOOP문
+    
+    [표현식]
+    LOOP
+        반복적으로 실행할 구문;
+        * 반복을 빠져나갈수 있는 구문;
+    END LOOP;
+    
+    - 반복문을 빠져나오는 구문
+      1). IF 조건식 THEN EXIT; END IF;
+      2). EXIT WHEN 조건식;
+*/
+
+-- 1~5까지 1씩 증가하면서 출력
+--  1). IF 조건식 이용으로 빠져나오기
+DECLARE
+    X NUMBER := 1;
+BEGIN
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(X);
+         X := X+1;
+         
+        IF X=6 THEN EXIT;
+        END IF;
+        
+    END LOOP;
+END;
+/
+
+-- 2). EXIT WHEN 조건식;
+DECLARE
+    X NUMBER := 1;
+BEGIN
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(X);
+         X := X+1;
+         
+        EXIT WHEN X=6;      
+    END LOOP;
+END;
+/
+
+--------------------------------------------------------------------------------------------------------------------
+/*
+    2. FOR LOOP문
+    
+    [표현식]
+    FOR 변수 IN [REVERSE] 초기값..최종값
+    LOOP
+        반복적으로 실행할 구문
+    END LOOP;
+*/
+
+BEGIN
+    FOR X IN 1..5
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(X);
+    END LOOP;
+END;
+/
+
+BEGIN
+    FOR X IN REVERSE 1..5
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(X);
+    END LOOP;
+END;
+/
+
+-- 테이블과 시퀀스를 생성하여
+-- INSERT하는 구문
+CREATE TABLE PLSQL (
+    TNO NUMBER PRIMARY KEY,
+    TDATE DATE
+);
+
+CREATE SEQUENCE SEQ_TNO
+INCREMENT BY 2
+NOCACHE;
+
+-- 100행 INSERT
+BEGIN
+    FOR X IN 1..100
+    LOOP
+        INSERT INTO PLSQL VALUES(SEQ_TNO.NEXTVAL, SYSDATE);
+    END LOOP;
+END;
+/
+
+--------------------------------------------------------------------------------------------------------------------
+/*
+    3. WHILE LOOP문
+        
+        [표현식]
+        WHILE 조건식
+        LOOP
+            반복적으로 실행할 구문;
+        END LOOP;
+*/
+
+DECLARE
+    X NUMER := 1;
+BEGIN
+    WHILE X < 6
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(X);
+        X := X+1;
+    END LOOP;
+END;
+/
+
+
+--------------------------------------------------------------------------------------------------------------------
+/*
+    <예외처리부>
+    
+    [표현식]
+    EXCEPTION
+        WHEN 예외명1 THEN 예외처리구문;
+        WHEN 예외명2 THEN 예외처리구문;
+        WHEN OTHERS THEN 예외처리구문;
+        
+        * 시스템 예외(오라클에서 미리 정의해둔 예외)
+         - NO_DATA_FOUND : SELECT한 결과가 한 행도 없을 경우
+         - TOO_MANY_ROWS : SELECT한 결과가 여러행일 경우
+         - ZERO_DIVIDE : 0으로 나눌 때
+         - DUP_VAL_ON_INDEX : UNIQUE 제약조건에 위배 되었을 때
+         ...
+*/
+DECLARE
+    RESULT NUMBER;
+BEGIN
+    RESULT := 10/&숫자;
+    DBMS_OUTPUT.PUT_LINE('결과 : ' || RESULT);
+EXCEPTION
+    WHEN ZERO_DIVIDE THEN DBMS_OUTPUT.PUT_LINE('0으로 나눌 수 없습니다');
+    -- WHEN OTHERS THEN DBMS_OUTPUT.PUT_LINE('0으로 나눌 수 없습니다');
+END;
+/
+
+-- UNIQUE 제약조건 위배
+BEGIN
+    UPDATE EMPLOYEE
+    SET EMP_ID = '&변경할사번'
+    WHERE EMP_NAME = '이정하';
+EXCEPTION
+    WHEN DUP_VAL_ON_INDEX THEN DBMS_OUTPUT.PUT_LINE('이미 존재하는 사번입니다');
+END;
+/
+
+-- 사수번호 200번은 여러명, 201번은 1명, 202번 한명도 없음
+DECLARE
+    EID EMPLOYEE.EMP_ID%TYPE;
+    ENAME EMPLOYEE.EMP_NAME%TYPE;
+BEGIN
+    SELECT EMP_ID, EMP_NAME
+    INTO EID, ENAME
+    FROM EMPLOYEE
+    WHERE MANAGER_ID = &사수사번;
+    
+    DBMS_OUTPUT.PUT_LINE('사번 : ' || EID);
+    DBMS_OUTPUT.PUT_LINE('이름 : ' || ENAME);
+EXCEPTION
+    WHEN TOO_MANY_ROWS THEN DBMS_OUTPUT.PUT_LINE('여러행을 조회할 수 없습니다.');
+    WHEN NO_DATA_FOUND THEN DBMS_OUTPUT.PUT_LINE('조회 결과가 없습니다.');
+END;
+/
+    
+------------------------------------------------------- 실습 문제 ------------------------------------------------------
+/*
+    1. 사원의 연봉을 구하는 PL/SQL 블럭 작성. 보너스가 있는 사원은 보너스도 포함(IF문으로)
+    2. 구구단 짝수단만 출력
+       2.1) FOR LOOP
+       2.2) WHILE LOOP
+*/
+    
+
+
+
+
